46892961867d7b6bc6e114ef5dc9e9a43f577024
mytiantian001 <2949036402@qq.com>
2026-08-15 14:06:25 +0800

    pearl: Add blobs from pearl OS3.0.3.0.VLHCNXM
    Device-specific subset (1055 blobs) after splitting the SoC-common
    blobs into vendor/xiaomi/mt6895-common. Includes the Dolby DAP
    engine ported from yuechu OS3.0.10.0 (dms@2.0 service, effect
    libraries, vintf manifest and prebuilt module definitions).

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),pearl)
+
+$(call add-radio-file-sha1-checked,radio/dtbo.img,29bf140fdc7ed2574602421fbff4452335f4a318)
+$(call add-radio-file-sha1-checked,radio/apusys.img,08d40d84a6306a2ae2455b0cdbca1f2f30c0ff13)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,e6cceb9c5b2a0e383810dde34c7c6f0890811d4e)
+$(call add-radio-file-sha1-checked,radio/ccu.img,02902fde8cd76c3f0787f6d80f5ddfadc4e0140f)
+$(call add-radio-file-sha1-checked,radio/dpm.img,2be91b1f9dd1c60948a583bc598805043db35d43)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,6e8b828fa812695d48473574f6a0566bb1557aa2)
+$(call add-radio-file-sha1-checked,radio/gz.img,648d355aef658cd7ee8df8bb7137310cdd6726c4)
+$(call add-radio-file-sha1-checked,radio/lk.img,47c60719bc6db8fbe099dea7af154d6e20ccd6d7)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,8ef5e345a69cab4d8fd6de9c006838cb3072bf97)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,da991a6b01eafe8ce65cb0b6f040d9b61f830280)
+$(call add-radio-file-sha1-checked,radio/md1img.img,5410af40467d3b981debe905e2fe9ea86199393f)
+$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,973f6b9cc429247517a28615b4f8d66172c76877)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,bcf000861b7540f5fe54430d5f2c36f8fa293021)
+$(call add-radio-file-sha1-checked,radio/scp.img,f90b1a948bce1f48f15889e3ec830572911d0505)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,1e030a8ef5c187bf2e7e0e34ed4d5c4dd3ea3ff1)
+$(call add-radio-file-sha1-checked,radio/sspm.img,6f04878886138d5acb7a43dbd3bc7662213b5ef1)
+$(call add-radio-file-sha1-checked,radio/tee.img,62111adf412230fd6348c15403880550e26ce69d)
+$(call add-radio-file-sha1-checked,radio/vcp.img,b5cf7b79156e2cec3cd00f98443e7a0feb314f6a)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,566b02654b368b655b6045c3fc48090c534c2422)
+$(call add-radio-file-sha1-checked,radio/logo.img,93126ccf572eaafa16704a999abd8f4c31dcf9c7)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+# Firmware partitions extracted from pearl-ota_full-OS3.0.3.0.VLHCNXM
+# (all hashes verified against device/xiaomi/pearl/proprietary-firmware.txt)
+AB_OTA_PARTITIONS += \
+    apusys \
+    audio_dsp \
+    ccu \
+    dpm \
+    dtbo \
+    gpueb \
+    gz \
+    lk \
+    logo \
+    mcf_ota \
+    mcupm \
+    md1img \
+    mvpu_algo \
+    pi_img \
+    preloader_raw \
+    scp \
+    spmfw \
+    sspm \
+    tee \
+    vcp
